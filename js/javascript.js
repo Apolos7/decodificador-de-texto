4f0ef4b90af8a1f5e73c8e6fcee7b3b7934b1ce0
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-31 12:32:01 -0300

    Projeto criado

---
+++ js/javascript.js
@@ -0,0 +1,55 @@
+const input = document.getElementById('input-text');
+const placeholder = document.getElementById('placeholder');
+const textOutputContainer = document.getElementById('text-output-container');
+const textOutput = document.getElementById('text-output');
+
+function criptografar() {
+    let text = input.value;
+
+    for (let i = 0; i < text.length; i++) {
+        if (text.charAt(i) == 'a') {
+            text = text.slice(0, i) + 'ai' + text.slice(i + 1, text.length);
+            i += 1;
+        } else if (text.charAt(i) == 'e') {
+            text = text.slice(0, i) + 'enter' + text.slice(i + 1, text.length);
+            i += 4;
+        } else if (text.charAt(i) == 'i') {
+            text = text.slice(0, i) + 'imes' + text.slice(i + 1, text.length);
+            i += 3;
+        } else if (text.charAt(i) == 'o') {
+            text = text.slice(0, i) + 'ober' + text.slice(i + 1, text.length);
+            i += 3;
+        } else if (text.charAt(i) == 'u') {
+            text = text.slice(0, i) + 'ufat' + text.slice(i + 1, text.length);
+            i += 3;
+        }
+    }
+    showResult(text);
+}
+
+function descriptografar() {
+    let text = input.value;
+
+    for (let i = 0; i < text.length; i++) {
+        if (text.slice(i, i + 2) == 'ai') {
+            text = text.slice(0, i) + 'a' + text.slice(i + 2, text.length);
+        } else if (text.slice(i, i + 5) == 'enter') {
+            text = text.slice(0, i) + 'e' + text.slice(i + 5, text.length);
+        } else if (text.slice(i, i + 4) == 'imes') {
+            text = text.slice(0, i) + 'i' + text.slice(i + 4, text.length);
+        } else if (text.slice(i, i + 4) == 'ober') {
+            text = text.slice(0, i) + 'o' + text.slice(i + 4, text.length);
+        } else if (text.slice(i, i + 4) == 'ufat') {
+            text = text.slice(0, i) + 'u' + text.slice(i + 4, text.length);
+        }
+    }
+
+    showResult(text);
+}
+
+
+function showResult(text) {
+    placeholder.style.display = "none";
+    textOutput.textContent = text;
+    textOutputContainer.style.display = 'flex';
+}
